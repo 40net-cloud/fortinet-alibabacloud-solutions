1c1398469321170dc6e6405fdaa1592d75feeeba
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2024-11-15 12:45:25 +0300

    Add files via upload

---
+++ FortiGate/Standalone/terraform/output.tf
@@ -0,0 +1,11 @@
+output "FGTPublicIP" {
+  value = alicloud_instance.Fortigate.public_ip
+}
+
+output "Username" {
+  value = "admin"
+}
+
+output "Password" {
+  value = alicloud_instance.Fortigate.id
+}
